195f8379520e581c3dafc7cf8a4913665d0c2642
Yuka O'oka <oukayuka@ayeate.com>
2018-09-18 19:52:11 +0900

    Add chapter 1

---
+++ 01-hello/src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">こんにちはReact</h1>
+        </header>
+        <p className="App-intro">
+          Hello, World!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
