e72ae19df48a0d2125f21df226dac7decffcce84
Yuka O'oka <oukayuka@ayeate.com>
2018-10-01 00:38:30 +0900

    Add comment literal chapter 5-2

--- 05-jsx/02-jsx/src/App.tsx
+++ 05-jsx/02-jsx/src/App.tsx
@@ -16,6 +16,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          {
+            // コメントはこう書く
+          }
           <img {...logoOptions} />
           {title && <h1 className="App-title">{title}</h1>}
         </header>
